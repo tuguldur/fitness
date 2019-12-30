6b65534624786139dc2f30ef3601ffe4be902557
tuguldur <aa402115@gmail.com>
2019-12-30 12:31:31 +0800

    Updates

---
+++ precache-manifest.90c0cf4a67be25f9e7bdb16d5d97cfd8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21cdf865f2507dd02ca3ff5b86d5f887",
+    "url": "/fitness/index.html"
+  },
+  {
+    "revision": "b309168e3e7d7fba8aae",
+    "url": "/fitness/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "115466108be43541984b",
+    "url": "/fitness/static/css/main.01bf7304.chunk.css"
+  },
+  {
+    "revision": "b309168e3e7d7fba8aae",
+    "url": "/fitness/static/js/2.64d52a6d.chunk.js"
+  },
+  {
+    "revision": "115466108be43541984b",
+    "url": "/fitness/static/js/main.7535b989.chunk.js"
+  },
+  {
+    "revision": "d2b896417475adc30d5f",
+    "url": "/fitness/static/js/runtime~main.98422cc9.js"
+  },
+  {
+    "revision": "a80cd7a77206b15ee13a582ea53b4cab",
+    "url": "/fitness/static/media/logo-white.a80cd7a7.svg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/fitness/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/fitness/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/fitness/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/fitness/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
